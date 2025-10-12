fd86b3549969fc16b083f1ac3a814872e8df165a
olympia15 <olympiamichelle15@gmail.com>
2025-10-12 19:26:31 -0400

    User model created

---
+++ models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true,
+        trim: true,
+        lowercase:true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: {
+            validator: function(v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: "Please enter a valid email."
+        }
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"]
+    }
+}, { 
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } 
+});
+    
+// hash password before saving
+userSchema.pre('save', async function(next) {
+    
+    if (!this.isModified("password")){
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+// method to compare password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
